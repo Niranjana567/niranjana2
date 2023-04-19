ffc0634263c4935c571245cd145cd71dbf190046
Niranjana567 <131260135+Niranjana567@users.noreply.github.com>
2023-04-19 18:25:00 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,26 @@
+#for j in range(6):
+ #   print("*"*j)
+
+# for i in range(6):
+#     for j in range(i,6):
+#         print("*",end="")
+# #     print()
+
+# def my_function(a,b,c):
+#   print(a+b+c)
+
+# a=int(input("enter first no :"))
+# b=int(input("enter second no :"))
+# c=int(input("enter third no :"))
+# my_function(a,b,c)
+
+# 
+
+# def my_function(a,b,c):
+#   print(a+b+c)
+
+# a=int(input("enter first no :"))
+# b=int(input("enter second no :"))
+# c=int(input("enter third no :"))
+# my_function(a,b,c)
+
